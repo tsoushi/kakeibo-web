8cc9a51ad26ae7c3225562d9dc0cd589be699ce6
muizu555 <muizu0920555@protonmail.com>
2025-05-09 20:58:57 +0900

    fix

--- server/migrate/seed.sql
+++ server/migrate/seed.sql
@@ -1,5 +1,5 @@
 -- record_type に初期データを挿入（支出、収入、振替）
-INSERT IGNORE INTO record_type (id, name) VALUES
-    (1, '支出'),
-    (2, '収入'),
-    (3, '振替');
+INSERT IGNORE INTO record_type (name) VALUES
+    ('EXPENSE'),
+    ('INCOME'),
+    ('TRANSFER');
